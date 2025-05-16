177d56ca60b9c28ad79d330c9b8e9f60b69fc949
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-16 16:41:58 +0200

    Add again the test
    i've should only change the comparision opeerator

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,6 @@
 const repeatString = function(string,repeat) {
 if(string == "") return "Add a string";
+if(repeat < 0) return "Add a number greater than 0";
 let newString=""
 for (let i=0; i<repeat;i++){
     newString+=string
