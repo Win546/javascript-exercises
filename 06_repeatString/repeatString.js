e2c97362a53b2aa5281f96bffdd0f62594cae9d2
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-16 16:27:04 +0200

    Fix another bug
    changed another comaprison tha lead to logic error

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -2,7 +2,7 @@ const repeatString = function(string,repeat) {
 if(string == "") return "Add a string";
 if(repeat <= 0) return "Add a number greater than 0";
 let newString=""
-for (let i=0; i<=repeat;i++){
+for (let i=0; i<repeat;i++){
     newString+=string
 }
 return newString
