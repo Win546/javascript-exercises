b3ddfcbf5159f26e6aa4b4c0133abd32c73eea7a
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-16 16:05:57 +0200

    Add the code
    i've created the code to achive what requested,  i've also handled error

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeat) {
+if(string == "") return "Add a string";
+if(repeat <= 0) return "Add a number greater than 0";
+let newString=""
+for (let i=0; i<=repeat;i++){
+    newString+string
+}
+return newString
 };
 
 // Do not edit below this line
