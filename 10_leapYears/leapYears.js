6095f41307de6660ff265616e75bf7ce426fac42
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-18 21:00:20 +0200

    Changed comparison operatoe
    i've chafnged the comparsion  == to ===

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) {
-if (year % 4 == 0){
- if (year % 100 == 0  ){
-    if ( year % 400 == 0 ){
+if (year % 4 === 0){
+ if (year % 100 === 0  ){
+    if ( year % 400 === 0 ){
         return true
     }
     return false
