76c5f8283eb390d2af8105e929075b27c1cbdd36
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-18 17:39:49 +0200

    Use array filer methos
    i've used a array filter methods to achive the goal of the excercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (array, ...args) => {
+    let newArray= [];
+   
+    for (let arg of args) {
+     newArray=array.filter( (element) => element !== arg)
+     array=newArray;
+}
+ 
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
