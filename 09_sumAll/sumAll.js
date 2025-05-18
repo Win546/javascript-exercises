d98b656bc6253b7a505981de8fa53a5018dc435f
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-18 18:33:13 +0200

    Add function to sum up numbers
    As requeste i've implemented a code to summ up all integer between two numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
-};
+    let sum=0;
+    for (let i=a;i<=b;i++){
+sum+=i
+
+;    }
+return sum;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
