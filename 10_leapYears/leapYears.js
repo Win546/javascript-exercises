f6fef160769a7c307633d0f1f340df436209440e
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-18 20:58:49 +0200

    Complete the code
    i've made all the code needed to achive all the step

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,7 +1,10 @@
 const leapYears = function(year) {
 if (year % 4 == 0){
- if (year % 100 == 0 && year % 400 == 0 ){
-    return true
+ if (year % 100 == 0  ){
+    if ( year % 400 == 0 ){
+        return true
+    }
+    return false
  }
  return true
 } 
