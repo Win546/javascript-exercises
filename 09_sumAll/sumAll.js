979952af3c54d11c846dc1340da3dbf108eb4c7e
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-18 19:03:12 +0200

    Add code to solve test  3
    i've  make a test to understan whic number it's greater

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,13 +1,12 @@
-const sumAll = function(a,b) {
-
-    let sum=0;
-    for (let i=a;i<=b;i++){
-sum+=i
-
-;    }
-return sum;
+const sumAll = function (a, b) {
+    if (a <= 0 || b <= 0) return "ERROR"
+    let max=a>b?a:b;
+    let min=a<b?a:b;
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 }
-
-
 // Do not edit below this line
 module.exports = sumAll;
