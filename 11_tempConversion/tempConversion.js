fa8f28bcbb82125944688943573273e810d822c0
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-19 19:37:06 +0200

    Handled deciaml
    i've adda a toFixed to get only one decimal and then parsefloat becuase toFixed return a string.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,6 +1,6 @@
-const convertToCelsius = (temp) => {return (temp - 32) / 1.8 };
+const convertToCelsius = (temp) => {return parseFloat(((temp - 32) / 1.8).toFixed(1)) };
 
-const convertToFahrenheit = (temp) => {return (temp * 1.8) + 32};
+const convertToFahrenheit = (temp) => {return parseFloat(((temp * 1.8) + 32).toFixed(1)) };
 
 // Do not edit below this line
 module.exports = {
