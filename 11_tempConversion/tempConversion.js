8669fc2d087cece263fa7282bc362e37940afc07
Win546 <91315687+Win546@users.noreply.github.com>
2025-05-19 19:31:04 +0200

    Add code to achive first test
    I've modify the funciton in arrow funciton and i added the formula to calculate celsius and fahrenheit

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,6 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = (temp) => {return (temp - 32) / 1.8 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = (temp) => {return (temp * 1.8) + 32};
 
 // Do not edit below this line
 module.exports = {
